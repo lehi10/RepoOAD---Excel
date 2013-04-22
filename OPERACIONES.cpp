8c8c5b8f07318be997602bcaafe0bbb116ab0c27
lehi10 <lehi_10@hotmail.com>
2013-04-21 20:39:54 -0500

    Create OPERACIONES.cpp

---
+++ OPERACIONES.cpp
@@ -0,0 +1,22 @@
+#include "OPERACIONES.h"
+
+OPERACIONES::OPERACIONES()
+{
+    //ctor
+}
+float OPERACIONES::suma(float a, float b)
+    {
+        return a+b;
+    }
+float OPERACIONES::resta(float a,float b)
+    {
+        return a-b;
+    }
+float OPERACIONES::multip(float a,float b)
+    {
+        return a*b;
+    }
+float OPERACIONES::divi(float a,float b)
+    {
+        return a/b;
+    }
