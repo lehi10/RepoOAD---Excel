340482e88b5265d8be8e1a5e96206b5379c293ac
lehi10 <lehi_10@hotmail.com>
2013-04-21 20:39:34 -0500

    Create OPERACIONES.h

---
+++ OPERACIONES.h
@@ -0,0 +1,20 @@
+#ifndef OPERACIONES_H
+#define OPERACIONES_H
+
+
+class OPERACIONES
+{
+    private:
+
+
+    public:
+        OPERACIONES();
+        float suma(float a, float b);
+        float resta(float a,float b);
+        float multip(float a,float b);
+        float divi(float a,float b);
+    protected:
+
+};
+
+#endif // OPERACIONES_H
