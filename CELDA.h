fed2709214d9c52ab338f4737639c8a16577425f
lehi10 <lehi_10@hotmail.com>
2013-04-21 20:33:47 -0500

    Create CELDA.h

---
+++ CELDA.h
@@ -0,0 +1,23 @@
+#include "CELDA.h"
+
+CELDA::CELDA()
+{
+    m_der=NULL;
+    m_abj=NULL;
+    m_fila=0;
+    m_colum=0;
+    m_valor=" ";//ctor
+}
+CELDA::CELDA(string val)
+{
+    m_valor=val;
+    m_der=NULL;
+    m_abj=NULL;
+    m_fila=0;
+    m_colum=0;
+}
+CELDA::~CELDA()
+{
+    delete m_der;
+    delete m_abj;
+}
