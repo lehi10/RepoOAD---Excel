136acd5d872ba6ff79ac63eddb4b4d4a05fb572b
nicolas001 <nnicorast@gmail.com>
2013-05-05 02:29:05 +0300

    Create string_c.cpp

---
+++ string_c.cpp
@@ -0,0 +1,15 @@
+#include "string_c.h"
+#include <iostream>
+string_c::string_c(string a)
+{
+    m_string_c=a;
+}
+double string_c :: eval()
+{
+    cout<<"error"<<endl;
+    return 0.0;
+}
+string_c::~string_c()
+{
+    //dtor
+}
