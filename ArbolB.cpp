bae12ae09f4a07331432732e061df799dbdc8798
nicolas001 <nnicorast@gmail.com>
2013-05-18 14:15:49 -0500

    Update ArbolB.cpp

--- ArbolB.cpp
+++ ArbolB.cpp
@@ -1,11 +1,14 @@
 #include "ArbolB.h"
 
-ArbolB::ArbolB(nodo * pn)
+ArbolB::ArbolB()
 {
-    m_head = pn; Hr=0;Hl=0;
+    headptr=0;
 }
-
+void ArbolB :: set_headptr(nodoTrue * mainhead)
+{
+    headptr=mainhead;
+}
 ArbolB::~ArbolB()
 {
-    //dtor
+
 }
